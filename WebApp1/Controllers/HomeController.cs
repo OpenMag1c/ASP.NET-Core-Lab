2fa69e4d936b67b307a30d2179d93b24e76c33fd
Rojkov Maxim <maximro.box@gmail.com>
2021-10-26 17:16:45 +0300

    Добавьте файлы проекта.

---
+++ WebApp1/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace WebApp1.Controllers
+{
+    public interface IMessageSender
+    {
+        string GetInfo();
+    }
+
+    public class HomeController
+    {
+        public string GetInfo() => "Hello, world!";
+    }
+
+    public class TextMessage : IMessageSender
+    {
+        public string GetInfo() => "Hello, world!";
+    }
+
+    public class MessageService
+    {
+        IMessageSender _sender;
+        public MessageService(IMessageSender sender)
+        {
+            _sender = sender;
+        }
+        public string Getinfo()
+        {
+            return _sender.GetInfo();
+        }
+    }
+
+    public class MessageMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public MessageMiddleware(RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, IMessageSender messageSender)
+        {
+            context.Response.ContentType = "text/html;charset=utf-8";
+            await context.Response.WriteAsync(messageSender.GetInfo());
+        }
+    }
+}
