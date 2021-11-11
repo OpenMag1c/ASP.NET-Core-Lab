fc0611448287c6baab968b365fcd0efd24bceb35
Rojkov Maxim <maximro.box@gmail.com>
2021-11-11 17:30:53 +0300

    task71

--- Business/DTO/ConfirmEmailDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Business.DTO
-{
-    public class ConfirmEmailDTO
-    {
-        public int Id { get; set; }
-        public string Token { get; set; }
-        public string Email { get; set; }
-    }
-}
