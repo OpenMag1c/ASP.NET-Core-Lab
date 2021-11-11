70e219cba5776bf7c3862abb5e822c5f3f03beb7
Rojkov Maxim <maximro.box@gmail.com>
2021-11-11 17:29:23 +0300

    userHelper

--- Business/Helper/UserHelpers.cs
+++ Business/Helper/UserHelpers.cs
@@ -10,5 +10,20 @@ namespace Business.Helper
         {
             return claims.Where(c => c.Type == "nameid").Select(c => c.Value).SingleOrDefault();
         }
+
+        public static string GetUserEmailByClaim(IEnumerable<Claim> claims)
+        {
+            return claims.Where(c => c.Type == ClaimTypes.Email).Select(c => c.Value).SingleOrDefault();
+        }
+
+        public static string GetUserNameByClaim(IEnumerable<Claim> claims)
+        {
+            return claims.Where(c => c.Type == ClaimTypes.Name).Select(c => c.Value).SingleOrDefault();
+        }
+
+        public static IEnumerable<string> GetUserRolesByClaim(IEnumerable<Claim> claims)
+        {
+            yield return claims.Where(c => c.Type == ClaimTypes.Role).Select(c => c.Value).SingleOrDefault();
+        }
     }
 }
